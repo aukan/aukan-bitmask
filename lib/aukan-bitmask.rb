32866b3143a804fcb816089fda597b125eeedcbc
Pablo Gonzalez <pbglezc@gmail.com>
2012-11-13 11:13:04 -0800

    Fix paths for console

--- lib/aukan-bitmask.rb
+++ lib/aukan-bitmask.rb
@@ -1,5 +1,5 @@
-require 'bitmask'
-require 'bitmask_attribute'
+require File.dirname(__FILE__) + '/bitmask'
+require File.dirname(__FILE__) + '/bitmask_attribute'
 
 module AukanBitmask
   VERSION = '0.0.1'
