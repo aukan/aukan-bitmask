c2cd71aae5351f5090006b8bfdd9d040f8afbac7
Pablo Gonzalez <pbglezc@gmail.com>
2012-11-13 08:36:00 -0800

    Require bitmask and bitmask_attribute libraries by default

--- lib/aukan-bitmask.rb
+++ lib/aukan-bitmask.rb
@@ -1,6 +1,9 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'lib/bitmask'
+require 'lib/bitmask_attribute'
+
 module AukanBitmask
   VERSION = '0.0.1'
 end
